db6574e777bdec122d0e545cba4e181c2cafd477
Skoubou9 <72798819+Skoubou9@users.noreply.github.com>
2021-05-01 18:19:46 +0100

    addded stuff

--- src/Graph.h
+++ src/Graph.h
@@ -3,8 +3,11 @@
 
 #include <vector>
 #include <climits>
+#include <limits>
 
 using namespace std;
+
+#define DOUBLE_MAX numeric_limits<double>::max();
 
 template <class T> class Edge;
 template <class T> class Node;
@@ -40,6 +43,7 @@ public:
     T getContents() const;
 
     void setContents(T contents);
+    vector<Edge<T>> getConnections();
 };
 
 template <class T>
@@ -51,6 +55,7 @@ public:
     Edge(Node<T> *dest, double weight);
     friend class Graph<T>;
     friend class Node<T>;
+    double getWeight();
 };
 
 template <class T>
@@ -61,6 +66,15 @@ private:
     vector<vector<int>> next;//to reconstruct the path after running the algorithm
 
     Node<T> *findNode(const T &contents) const;
+    /**
+     * @brief Sets visited to false for all nodes
+     */
+    void resetVisited();
+    /**
+     * @brief Depth-first search on graph
+     * @param source Node to start dfs in
+     */
+    void dfs(const T &source);
 public:
     /**
      * @return Number of nodes in the graph
@@ -93,15 +107,226 @@ public:
      * @return true if edge existed and was removed, false otherwise
      */
     bool removeEdge(const T &source, const T &dest);
+    std::vector<Node<T>*> getNodeSet() const;
 
-    bool removeUnnecessaryEdges(const T &source);
+    void removeUnnecessaryEdges(const T &source);
 
     double edgeCost(int i, int j);
     int nodePrev(int i, int j);
     void floydWarshallShortestPath();
-    vector<Node<T> * > getFloydWarshallPath(Node<T> * u, Node<T> * v);
+    std::vector<Node<T> * > getFloydWarshallPath(Node<T> * u, Node<T> * v);
     Node<T> * operator[](int n);
 };
+
+template<class T>
+void Node<T>::addEdge(Node<T> *dest, double weight) {
+    connections.push_back(Edge<T>(dest, weight));
+}
+
+template<class T>
+bool Node<T>::removeEdgeTo(Node<T> *dest) {
+    for (auto it = connections.begin(); it != connections.end(); it++) {
+        //if edge destination equals dest remove it and return true
+        if (it->dest == dest) {
+            connections.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+template<class T>
+Node<T>::Node(T contents): contents(contents), visited(false), processing(false), indegree(0) {}
+
+template<class T>
+T Node<T>::getContents() const {
+    return contents;
+}
+
+template<class T>
+void Node<T>::setContents(T contents) {
+    Node::contents = contents;
+}
+
+template<class T>
+vector<Edge<T>> Node<T>::getConnections() {
+    return connections;
+}
+
+template<class T>
+Edge<T>::Edge(Node<T> *dest, double weight) {
+    this->dest = dest;
+    this->weight = weight;
+}
+
+template<class T>
+double Edge<T>::getWeight(){
+    return weight;
+}
+
+template<class T>
+Node<T> *Graph<T>::findNode(const T &contents) const {
+    for (auto node : nodeSet) if (node->contents == contents) return node;
+    return nullptr;
+}
+
+template<class T>
+void Graph<T>::resetVisited() {
+    for(auto node : nodeSet) node->visited = false;
+}
+
+template<class T>
+unsigned int Graph<T>::getNumNodes() const {
+    return nodeSet.size();
+}
+
+template<class T>
+bool Graph<T>::addNode(const T &contents) {
+    //if node doesn't exist add it and return true
+    if (findNode(contents) == nullptr) {
+        nodeSet.push_back(new Node<T>(contents));
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+bool Graph<T>::removeNode(const T &contents) {
+    Node<T> *targetNode;
+    //if node exists
+    if ((targetNode = findNode(contents)) != nullptr) {
+        //remove all edges to target node
+        for (auto node : nodeSet) node->removeEdgeTo(targetNode);
+        //free target node memory allocated in addNode() and remove it from nodeSet
+        for (auto it = nodeSet.begin(); it != nodeSet.end(); it++)
+            if (*it == targetNode) {
+                delete targetNode;
+                nodeSet.erase(it);
+                return true;
+            }
+        return false;
+    }
+    return false;
+}
+
+template<class T>
+bool Graph<T>::addEdge(const T &source, const T &dest, double weight) {
+    Node<T> *sourceTargetNode, *destTargetNode;
+    if ((sourceTargetNode = findNode(source)) != nullptr && (destTargetNode = findNode(dest)) != nullptr) {
+        sourceTargetNode->addEdge(destTargetNode, weight);
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+bool Graph<T>::removeEdge(const T &source, const T &dest) {
+    Node<T> *sourceTargetNode, *destTargetNode;
+    if ((sourceTargetNode = findNode(source)) != nullptr && (destTargetNode = findNode(dest)) != nullptr) {
+        return sourceTargetNode->removeEdgeTo(destTargetNode);
+    }
+    return false;
+}
+
+template<class T>
+vector<Node<T>*> Graph<T>::getNodeSet() const {
+    return nodeSet;
+}
+
+template<class T>
+void Graph<T>::removeUnnecessaryEdges(const T &source){
+    resetVisited();
+    dfs(source);
+    for(auto node : nodeSet){
+        if (!node->visited){
+            for(auto edge : node->connections){
+                edge.weight = DOUBLE_MAX;
+            }
+        }
+    }
+}
+
+template<class T>
+void Graph<T>::dfs(const T &source){
+    auto node = findNode(source);
+    node->visited = true;
+
+    for(auto edge : node->connections){
+        if(!(edge.dest->visited)){
+            dfs(source);
+        }
+    }
+}
+
+template<class T>
+void Graph<T>::floydWarshallShortestPath() {
+    int N = nodeSet.size(); //size of the square matrix
+    dist = vector<vector<double> >(N, vector<double>(N));
+    next = vector<vector<int> >(N, vector<int>(N));
+
+    //first step, set weight and previous
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            dist[i][j] = edgeCost(i, j);
+            next[i][j] = nodePrev(i, j);
+        }
+
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < N; k++) {
+        for (int j = 0; j < N; j++) {
+            for (int i = 0; i < N; i++) {
+                if (dist[i][j] > (dist[i][k] + dist[k][j])) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                    next[i][j] = next[i][k];
+                }
+            }
+        }
+    }
+}
+
+template<class T>
+double Graph<T>::edgeCost(int i, int j) {
+    if (i == j)
+        return 0;
+
+    for (auto edge : nodeSet[i]->connections)
+        if (edge.dest == nodeSet[j])
+            return edge.weight;
+
+    return INT_MAX;
+}
+
+template<class T>
+int Graph<T>::nodePrev(int i, int j) {
+    for (auto edge : nodeSet[i]->connections) {
+        if (edge.dest == nodeSet[j])
+            return j;
+    }
+
+    return -1;
+}
+
+template<class T>
+vector<Node<T> *> Graph<T>::getFloydWarshallPath(Node<T> *src, Node<T> *dest) {
+    vector<Node<T> * > result;
+    int found = 0;
+    int v = src->posAtVec, w = dest->posAtVec;
+
+    result.push_back(nodeSet[v]);
+    while (v != w) {
+        v = next[v][w];
+        if (v < 0)
+            break;
+        result.push_back(nodeSet[v]);
+    }
+    return result;
+}
+
+template<class T>
+Node<T> *Graph<T>::operator[](int n) {
+    return nodeSet[n];
+}
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include "Graph.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Not working" << std::endl;
+    Graph<int> myGraph;
+
+    for(int i = 1; i < 8; i++)
+        myGraph.addNode(i);
+
+    myGraph.addEdge(1, 2, 3);
+    myGraph.addEdge(1, 3, 2);
+    myGraph.addEdge(2, 5, 4);
+    myGraph.addEdge(2, 4, 3);
+    myGraph.addEdge(2, 3, 1);
+    myGraph.addEdge(3, 5, 2);
+    myGraph.addEdge(4, 6, 2);
+    myGraph.addEdge(5, 6, 3);
+
+    myGraph.removeUnnecessaryEdges(1);
+
+    for(auto node : myGraph.getNodeSet()){
+        for(auto edge : node->getConnections()){
+            cout << edge.getWeight() << endl;
+        }
+    }
     return 0;
 }
